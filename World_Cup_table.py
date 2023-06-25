9905d46e5f2c0fe114e775645901c6b7b8726285
Mohammad Ali <112762827+MA-HN@users.noreply.github.com>
2023-06-25 09:55:21 +0330

    get result of matches

---
+++ World_Cup_table.py
@@ -0,0 +1,8 @@
+
+country = ['Spain','Iran','Portugal','Morocco']
+content_list = ['wins:' , 'loses:' , 'draws:' , 'goal difference:' , 'points:']
+
+for i in range(0,6):
+    match_result_string = input()
+    
+
