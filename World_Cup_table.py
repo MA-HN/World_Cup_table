57cb4ed563ddd92c6648a6e512e3b035eaaac9b1
Mohammad Ali <112762827+MA-HN@users.noreply.github.com>
2023-06-25 11:36:14 +0330

    change the structure of inpute loop

--- World_Cup_table.py
+++ World_Cup_table.py
@@ -1,8 +1,9 @@
 
-country = ['Spain','Iran','Portugal','Morocco']
+country = ['Iran','Spain','Portugal','Morocco']
 content_list = ['wins:' , 'loses:' , 'draws:' , 'goal difference:' , 'points:']
 
-for i in range(0,6):
-    match_result_string = input()
+for i in range(0,3):
+    for j in range(0,i):
+        match_result_string = input()
     
 
